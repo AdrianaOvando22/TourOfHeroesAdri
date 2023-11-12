62b2acfeada16c95b32b30d79d0afec1dbc10f34
Adri <adriana.ovando@ucb.edu.bo>
2023-11-11 22:00:07 -0400

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
+  ],
+})
+export class HeroesComponent {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
